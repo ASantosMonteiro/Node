1bcee2bde27b218976046f50dd016e494ef97eba
Ariel <a.santos.monteiro@accenture.com>
2023-03-28 17:22:12 -0300

    Seccion 6 completa

--- Section-06/helpers/inquirer.js
+++ Section-06/helpers/inquirer.js
@@ -68,15 +68,15 @@ const leerInput = async( message) => {
 
 }
 
-const listadoTareasBorrar = async( tareas = [] ) => {
+const listarLugares = async( lugares = [] ) => {
 
-    const choices = tareas.map( (tarea, i) => {
+    const choices = lugares.map( (lugar, i) => {
 
         const idx = `${ i + 1 }`.green;
 
         return{
-            value: tarea.id,
-            name: `${ idx } ${tarea.desc}`
+            value: lugar.id,
+            name: `${ idx } ${lugar.nombre}`
         }
         
     });
@@ -90,7 +90,7 @@ const listadoTareasBorrar = async( tareas = [] ) => {
         {
             type: 'list',
             name: 'id',
-            message: 'Borrar',
+            message: 'Seleccione un lugar: ',
             choices
         }
     ]
@@ -150,7 +150,7 @@ export {
     inquirerMenu,
     pausa,
     leerInput,
-    listadoTareasBorrar,
+    listarLugares,
     confirmar,
     mostrarListadoChecklist
 }
--- Section-06/index.js
+++ Section-06/index.js
@@ -1,11 +1,15 @@
-import { inquirerMenu, leerInput, pausa } from "./helpers/inquirer.js"
-
+import { inquirerMenu, leerInput, listarLugares, pausa } from "./helpers/inquirer.js"
+import { Busquedas} from "./models/busquedas.js";
+import axios, {isCancel, AxiosError} from 'axios';
+import * as dotenv from 'dotenv' 
+dotenv.config()
 
 
 const main = async() => {
 
     let opt = 0 ;
 
+    const busquedas = new Busquedas();
 
     do{
         opt = await inquirerMenu();
@@ -13,18 +17,40 @@ const main = async() => {
         switch(opt){
             case 1:
                 // Buscar por ciudad
-                const ciudad = await leerInput('Ciudad:');
-                console.log(ciudad);
+                const lugar = await leerInput('Ciudad: ');
+                
+                const lugares = await busquedas.ciudad(lugar);
+                const id = await listarLugares(lugares);
+                if( id === '0' ) continue;
+
+                const lugarSel = lugares.find( l => l.id === id ); 
+                busquedas.agregarHistorial(lugarSel.nombre);
+                const clima = await busquedas.climaLugar( lugarSel.lat, lugarSel.lng );
+
+                console.clear();
+                console.log('\nInformacion de la ciudad\n'.green);
+                console.log('Ciudad:',lugarSel.nombre);
+                console.log('lat:',lugarSel.lat);
+                console.log('lng:',lugarSel.lng);
+                console.log('Temperatura:',clima.temp);
+                console.log('Minima:',clima.min);
+                console.log('Maxima:',clima.max);
+                console.log('El Clima esta: ',clima.desc)
+
             break;
 
             case 2:
+                // Mostrar historial
+                busquedas.historialCapitalizado.forEach( (lugar, i) => {
+                    const idx = `${ i+1 }.`.green;
+                    console.log(`${ idx } ${ lugar }` );
 
+                });
             break;
 
         }
 
-        console.log(opt);
-        await pausa();
+        if(opt !== 0) await pausa();
 
     }while(opt !== 0);
 }
