6a861cb13133e5ed74b8327bd4258cad60157d39
Ariel <a.santos.monteiro@accenture.com>
2023-04-14 09:04:47 -0300

    Seccion 11 completa

---
+++ Section-11/models/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
+
+
+class Server {
+
+    constructor() {
+
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+        this.authPath = '/api/auth';
+
+        this.conectarDB();
+
+        this.middlewares();
+
+        this.routes();
+
+    }
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+    middlewares() {
+
+        this.app.use( cors());
+
+        this.app.use(express.json());
+
+        this.app.use( express.static('public'));
+    }
+
+    routes(){
+
+        this.app.use(this.usuariosPath, require('../routes/user'));
+        this.app.use(this.authPath, require('../routes/auth'));
+
+    }
+
+    listen() {
+
+        this.app.listen(this.port, () => {
+            console.log(`Example app listening on port ${this.port}`)
+          });
+
+    }
+
+}
+
+
+module.exports = Server;
